d67ee0b3d61a245b975e40f7f68d6793ea30ec30
Beatriz Ventura <ventu@Ventura>
2025-07-23 11:01:16 -0300

    organizei minha pasta de estudos e atualizei o arquivo da index

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function armazenarValor() {
-    var userInput = document.getElementById('userInput').value;
-
-    if (userInput) {
-        document.getElementById('mostrarMensagem').innerText = "Parabéns, " + userInput + "! Você clicou no botão!";
-    } else {
-        document.getElementById('mostrarMensagem').innerText = 'Por favor, escreva seu nome antes de continuar.';
-    }
-}
